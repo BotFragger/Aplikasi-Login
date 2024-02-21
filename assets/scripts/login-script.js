a5fd5a114607818d034d8056361e522dc0759f36
BotFragger <86841850+BotFragger@users.noreply.github.com>
2024-02-21 13:22:27 +0700

    Update login-script.js
    adding comment

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -5,21 +5,29 @@
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
 
+/** variable elemen view */
 const loginFormElement = document.querySelector('#loginForm');
 const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+/** variabel menyimpan informasi email & password */
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
+/**button click action */
 loginFormElement.addEventListener('submit', function(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  const email = inputEmailElement.value;
-  const password = inputPasswordElement.value;
-  if (email == expectedEmail && password == expectedPassword) {
-    goToHome();
-  } else {
-    showPopUp();
-  }
+    /**get value dari input */
+    const email = inputEmailElement.value;
+    const password = inputPasswordElement.value;
+
+    /**membandingkan nilai */
+    if (email == expectedEmail && password == expectedPassword) {
+      //kondisi berhasil
+      goToHome();
+    } else {
+      //kondisi gagal
+      showPopUp();
+    }
 });
